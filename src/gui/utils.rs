0794c6956ad942877ad5f289beb8f9a0ed0a1607
JL710 <76447362+JL710@users.noreply.github.com>
2024-04-16 22:20:19 +0200

    move entry container style into utils

--- src/gui/utils.rs
+++ src/gui/utils.rs
@@ -10,3 +10,14 @@ pub fn labeled_entry<'a, Message: 'a + Clone>(
     .spacing(10)
     .into()
 }
+
+pub fn entry_row_container_style(theme: &iced::Theme) -> iced::widget::container::Style {
+    match theme {
+        iced::Theme::Dark => iced::widget::container::Style::default().with_background(
+            iced::Background::Color(iced::Color::from_rgb8(100, 100, 100)),
+        ),
+        _ => iced::widget::container::Style::default().with_background(iced::Background::Color(
+            iced::Color::from_rgb8(100, 100, 100),
+        )),
+    }
+}
