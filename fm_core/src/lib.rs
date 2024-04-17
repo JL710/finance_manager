fa552d8b80f0a7f26714cc4c7ee66459654b95e6
JL710 <example@examples.com>
2024-04-17 18:58:28 +0200

    move account mod into its own file

--- fm_core/src/lib.rs
+++ fm_core/src/lib.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 
+pub mod account;
+
 pub type DateTime = chrono::DateTime<chrono::Utc>;
 pub type Id = u128;
 
@@ -75,118 +77,6 @@ impl Budget {
 impl std::fmt::Display for Budget {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", &self.name)
-    }
-}
-
-pub mod account {
-    use super::{Currency, Id};
-
-    #[derive(Debug, Clone)]
-    pub struct AssetAccount {
-        id: Id,
-        name: String,
-        notes: Option<String>,
-        iban: Option<String>,
-        bic: Option<String>,
-    }
-
-    impl AssetAccount {
-        pub fn new(
-            id: Id,
-            name: String,
-            note: Option<String>,
-            iban: Option<String>,
-            bic: Option<String>,
-        ) -> Self {
-            Self {
-                id,
-                name,
-                notes: note,
-                iban,
-                bic,
-            }
-        }
-
-        pub fn id(&self) -> Id {
-            self.id
-        }
-
-        pub fn name(&self) -> &str {
-            &self.name
-        }
-
-        pub fn note(&self) -> Option<&str> {
-            match &self.notes {
-                Some(note) => Some(note),
-                None => None,
-            }
-        }
-
-        pub fn iban(&self) -> Option<&str> {
-            match &self.iban {
-                Some(content) => Some(content),
-                None => None,
-            }
-        }
-
-        pub fn bic(&self) -> Option<&str> {
-            match &self.bic {
-                Some(content) => Some(content),
-                None => None,
-            }
-        }
-    }
-
-    impl From<AssetAccount> for Account {
-        fn from(value: AssetAccount) -> Self {
-            Account::AssetAccount(value)
-        }
-    }
-
-    #[derive(Debug, Clone)]
-    pub enum BookChecking {
-        Expense,
-        Revenue,
-    }
-
-    #[derive(Debug, Clone)]
-    pub struct BookCheckingAccount {
-        id: Id,
-        name: String,
-        notes: Option<String>,
-        iban: Option<String>,
-        bic: Option<String>,
-        checking_type: BookChecking,
-    }
-
-    #[derive(Debug, Clone)]
-    pub enum Account {
-        AssetAccount(AssetAccount),
-        BookCheckingAccount(BookCheckingAccount),
-    }
-
-    impl PartialEq for Account {
-        fn eq(&self, other: &Self) -> bool {
-            match self {
-                Account::AssetAccount(acc) => match other {
-                    Account::AssetAccount(other_acc) => acc.id == other_acc.id,
-                    _ => false,
-                },
-                Account::BookCheckingAccount(acc) => match other {
-                    Account::BookCheckingAccount(other_acc) => acc.id == other_acc.id,
-                    _ => false,
-                },
-            }
-        }
-    }
-
-    impl PartialEq<Id> for Account {
-        fn eq(&self, other: &Id) -> bool {
-            match self {
-                Account::AssetAccount(acc) => acc.id == *other,
-                Account::BookCheckingAccount(acc) => acc.id == *other,
-            }
-        }
     }
 }
 
