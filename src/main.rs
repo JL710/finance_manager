af9d5c078fd2bf208bb3c88cd3a6ab4922ce136f
JL710 <76447362+JL710@users.noreply.github.com>
2024-04-17 08:44:27 +0200

    choose between server and gui via cli

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
 fn main() {
-    fm_server::run();
-    fm_gui::run();
+    let args = std::env::args().collect::<Vec<String>>();
+    if args.len() != 2 {
+        panic!("Please provide a command line argument to specify the mode of the application.");
+    }
+
+    match args[1].as_str() {
+        "server" => fm_server::run(),
+        "gui" => fm_gui::run(),
+        _ => panic!("Invalid mode specified."),
+    }
 }
