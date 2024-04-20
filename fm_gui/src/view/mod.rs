38f438d8d0b60f850135a9e35badc454e95c80a0
JL710 <example@examples.com>
2024-04-20 22:31:25 +0200

    re add show asset accounts view

--- fm_gui/src/view/mod.rs
+++ fm_gui/src/view/mod.rs
@@ -2,5 +2,5 @@ pub mod budget_overview;
 pub mod create_asset_account;
 pub mod create_budget;
 pub mod create_transaction;
-//pub mod show_asset_accounts;
+pub mod show_asset_accounts;
 //pub mod view_account;
