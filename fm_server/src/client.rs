4f517f90f5f8448c1667a497d4ca343be149da32
JL710 <76447362+JL710@users.noreply.github.com>
2024-04-22 17:53:58 +0200

    simplify client with macro

--- fm_server/src/client.rs
+++ fm_server/src/client.rs
@@ -9,6 +9,21 @@ impl Client {
     }
 }
 
+#[macro_export]
+macro_rules! client_post_macro {
+    ( $url:expr, $path:expr, $x:expr ) => {{
+        let client = reqwest::Client::new();
+        let response = client
+            .post(&format!("{}/{}", $url, $path))
+            .body(serde_json::json!($x).to_string())
+            .header(reqwest::header::CONTENT_TYPE, "application/json")
+            .send()
+            .await
+            .unwrap();
+        serde_json::from_str(&response.text().await.unwrap()).unwrap()
+    }};
+}
+
 impl fm_core::FinanceManager for Client {
     async fn create_asset_account(
         &mut self,
@@ -17,15 +32,7 @@ impl fm_core::FinanceManager for Client {
         iban: Option<String>,
         bic: Option<String>,
     ) -> fm_core::account::AssetAccount {
-        let client = reqwest::Client::new();
-        let response = client
-            .post(&format!("{}/create_asset_account", self.url))
-            .body(serde_json::json!((name, note, iban, bic)).to_string())
-            .header(reqwest::header::CONTENT_TYPE, "application/json")
-            .send()
-            .await
-            .unwrap();
-        serde_json::from_str(&response.text().await.unwrap()).unwrap()
+        client_post_macro!(self.url, "create_asset_account", (name, note, iban, bic))
     }
 
     async fn get_accounts(&self) -> Vec<fm_core::account::Account> {
@@ -36,16 +43,7 @@ impl fm_core::FinanceManager for Client {
     }
 
     async fn get_account(&self, id: fm_core::Id) -> Option<fm_core::account::Account> {
-        let client = reqwest::Client::new();
-        let response = client
-            .post(&format!("{}/get_account", self.url))
-            .header(reqwest::header::CONTENT_TYPE, "application/json")
-            .body(serde_json::json!(id).to_string())
-            .send()
-            .await
-            .unwrap();
-
-        serde_json::from_str(&response.text().await.unwrap()).unwrap()
+        client_post_macro!(self.url, "get_account", id)
     }
 
     async fn get_account_sum(
@@ -53,27 +51,11 @@ impl fm_core::FinanceManager for Client {
         account: &fm_core::account::Account,
         date: fm_core::DateTime,
     ) -> fm_core::Currency {
-        let client = reqwest::Client::new();
-        let response = client
-            .post(&format!("{}/get_account_sum", &self.url))
-            .body(serde_json::json!((account, date)).to_string())
-            .header(reqwest::header::CONTENT_TYPE, "application/json")
-            .send()
-            .await
-            .unwrap();
-        serde_json::from_str(&response.text().await.unwrap()).unwrap()
+        client_post_macro!(self.url, "get_account_sum", (account, date))
     }
 
     async fn get_transaction(&self, id: fm_core::Id) -> Option<fm_core::Transaction> {
-        let client = reqwest::Client::new();
-        let response = client
-            .post(&format!("{}/get_transaction", &self.url))
-            .body(serde_json::json!(id).to_string())
-            .header(reqwest::header::CONTENT_TYPE, "application/json")
-            .send()
-            .await
-            .unwrap();
-        serde_json::from_str(&response.text().await.unwrap()).unwrap()
+        client_post_macro!(self.url, "get_transaction", id)
     }
 
     async fn get_transactions_of_account(
@@ -81,15 +63,7 @@ impl fm_core::FinanceManager for Client {
         account: fm_core::Id,
         timespan: fm_core::Timespan,
     ) -> Vec<fm_core::Transaction> {
-        let client = reqwest::Client::new();
-        let response = client
-            .post(&format!("{}/get_transactions_of_account", &self.url))
-            .body(serde_json::json!((account, timespan)).to_string())
-            .header(reqwest::header::CONTENT_TYPE, "application/json")
-            .send()
-            .await
-            .unwrap();
-        serde_json::from_str(&response.text().await.unwrap()).unwrap()
+        client_post_macro!(self.url, "get_transactions_of_account", (account, timespan))
     }
 
     async fn create_transaction(
@@ -102,26 +76,19 @@ impl fm_core::FinanceManager for Client {
         budget: Option<fm_core::Id>,
         date: fm_core::DateTime,
     ) -> fm_core::Transaction {
-        let client = reqwest::Client::new();
-        let response = client
-            .post(&format!("{}/create_transaction", &self.url))
-            .body(
-                serde_json::json!((
-                    amount,
-                    title,
-                    description,
-                    source,
-                    destination,
-                    budget,
-                    date
-                ))
-                .to_string(),
+        client_post_macro!(
+            self.url,
+            "create_transaction",
+            (
+                amount,
+                title,
+                description,
+                source,
+                destination,
+                budget,
+                date
             )
-            .header(reqwest::header::CONTENT_TYPE, "application/json")
-            .send()
-            .await
-            .unwrap();
-        serde_json::from_str(&response.text().await.unwrap()).unwrap()
+        )
     }
 
     async fn create_book_checking_account(
@@ -131,15 +98,11 @@ impl fm_core::FinanceManager for Client {
         iban: Option<String>,
         bic: Option<String>,
     ) -> fm_core::account::BookCheckingAccount {
-        let client = reqwest::Client::new();
-        let response = client
-            .post(&format!("{}/create_book_checking_account", &self.url))
-            .body(serde_json::json!((name, notes, iban, bic,)).to_string())
-            .header(reqwest::header::CONTENT_TYPE, "application/json")
-            .send()
-            .await
-            .unwrap();
-        serde_json::from_str(&response.text().await.unwrap()).unwrap()
+        client_post_macro!(
+            self.url,
+            "create_book_checking_account",
+            (name, notes, iban, bic)
+        )
     }
 
     async fn create_budget(
@@ -149,15 +112,11 @@ impl fm_core::FinanceManager for Client {
         total_value: fm_core::Currency,
         timespan: fm_core::Recouring,
     ) -> fm_core::Budget {
-        let client = reqwest::Client::new();
-        let response = client
-            .post(&format!("{}/create_budget", self.url))
-            .body(serde_json::json!((name, description, total_value, timespan)).to_string())
-            .header(reqwest::header::CONTENT_TYPE, "application/json")
-            .send()
-            .await
-            .unwrap();
-        return serde_json::from_str(&response.text().await.unwrap()).unwrap();
+        client_post_macro!(
+            self.url,
+            "create_budget",
+            (name, description, total_value, timespan)
+        )
     }
 
     async fn get_budgets(&self) -> Vec<fm_core::Budget> {
@@ -171,14 +130,6 @@ impl fm_core::FinanceManager for Client {
         &self,
         budget: &fm_core::Budget,
     ) -> Vec<fm_core::Transaction> {
-        let client = reqwest::Client::new();
-        let response = client
-            .post(&format!("{}/get_transactions_of_budget", self.url))
-            .body(serde_json::json!(budget).to_string())
-            .header(reqwest::header::CONTENT_TYPE, "application/json")
-            .send()
-            .await
-            .unwrap();
-        return serde_json::from_str(&response.text().await.unwrap()).unwrap();
+        client_post_macro!(self.url, "get_transactions_of_budget", budget)
     }
 }
