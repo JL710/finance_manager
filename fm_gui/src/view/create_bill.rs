bad170c62c2a998d9a3713114b3785f77f38a59a
JL710 <76447362+JL710@users.noreply.github.com>
2024-07-19 13:06:05 +0200

    restructure add transaction in create bill page

--- fm_gui/src/view/create_bill.rs
+++ fm_gui/src/view/create_bill.rs
@@ -24,12 +24,11 @@ pub enum Message {
     ValueChanged(Option<fm_core::Currency>),
     DescriptionInputChanged(utils::multiline_text_input::Action),
     AddTransactionToggle,
-    AddTransactionFilterSubmit(fm_core::transaction_filter::TransactionFilter),
-    AddTransactionReloadTransactions(Vec<fm_core::Transaction>),
-    FetchedAddTransaction(AddTransaction),
     AddTransaction(fm_core::Transaction),
     ChangeTransactionSign(fm_core::Id, fm_core::Sign),
     RemoveTransaction(fm_core::Id),
+    AddTransactionMessage(add_transaction::Message),
+    FetchedAddTransaction(add_transaction::AddTransaction),
     Submit,
 }
 
@@ -41,7 +40,7 @@ pub struct CreateBillView {
     value: Option<fm_core::Currency>,
     due_date: Option<fm_core::DateTime>,
     transactions: Vec<(fm_core::Transaction, fm_core::Sign)>,
-    add_transaction: Option<AddTransaction>,
+    add_transaction: Option<add_transaction::AddTransaction>,
 }
 
 impl CreateBillView {
@@ -177,10 +176,18 @@ impl CreateBillView {
                     self.add_transaction = None;
                     return (None, iced::Task::none());
                 }
+                let ignored_transactions = self.transactions.iter().map(|x| *x.0.id()).collect();
                 return (
                     None,
                     iced::Task::perform(
-                        async { AddTransaction::fetch(finance_manager).await.unwrap() },
+                        async move {
+                            add_transaction::AddTransaction::fetch(
+                                finance_manager,
+                                ignored_transactions,
+                            )
+                            .await
+                            .unwrap()
+                        },
                         |x| AppMessage::CreateBillMessage(Message::FetchedAddTransaction(x)),
                     ),
                 );
@@ -188,47 +195,9 @@ impl CreateBillView {
             Message::FetchedAddTransaction(add_transaction) => {
                 self.add_transaction = Some(add_transaction);
             }
-            Message::AddTransactionFilterSubmit(filter) => {
-                if let Some(add_transaction) = &mut self.add_transaction {
-                    add_transaction.filter = None;
-                }
-                return (
-                    None,
-                    iced::Task::perform(
-                        async move {
-                            let locked_manager = finance_manager.lock().await;
-                            locked_manager
-                                .get_filtered_transactions(filter.clone())
-                                .await
-                                .unwrap()
-                        },
-                        |x| {
-                            AppMessage::CreateBillMessage(
-                                Message::AddTransactionReloadTransactions(x),
-                            )
-                        },
-                    ),
-                );
-            }
-            Message::AddTransactionReloadTransactions(transactions) => {
-                if let Some(add_transaction) = &mut self.add_transaction {
-                    let mut transactions = transactions;
-                    transactions.retain(|x| {
-                        for (t, _) in &self.transactions {
-                            if t.id() == x.id() {
-                                return false;
-                            }
-                        }
-                        true
-                    });
-
-                    add_transaction.transactions = transactions;
-                }
-            }
             Message::AddTransaction(transaction) => {
                 self.transactions
                     .push((transaction, fm_core::Sign::Positive));
-                self.add_transaction = None;
             }
             Message::ChangeTransactionSign(transaction_id, sign) => {
                 self.transactions
@@ -240,13 +209,35 @@ impl CreateBillView {
             Message::RemoveTransaction(transaction_id) => {
                 self.transactions.retain(|x| *x.0.id() != transaction_id);
             }
+            Message::AddTransactionMessage(m) => {
+                if let Some(add_transaction) = &mut self.add_transaction {
+                    match add_transaction.update(m, finance_manager) {
+                        add_transaction::Action::Escape => {
+                            self.add_transaction = None;
+                        }
+                        add_transaction::Action::AddTransaction(transaction) => {
+                            self.transactions
+                                .push((transaction, fm_core::Sign::Positive));
+                        }
+                        add_transaction::Action::Task(task) => {
+                            return (
+                                None,
+                                task.map(|x| {
+                                    AppMessage::CreateBillMessage(Message::AddTransactionMessage(x))
+                                }),
+                            );
+                        }
+                        add_transaction::Action::None => {}
+                    }
+                }
+            }
         }
         (None, iced::Task::none())
     }
 
     pub fn view(&self) -> iced::Element<Message> {
         if let Some(add_transaction) = &self.add_transaction {
-            return add_transaction.view();
+            return add_transaction.view().map(Message::AddTransactionMessage);
         }
 
         widget::column![
@@ -324,76 +315,144 @@ impl CreateBillView {
     }
 }
 
-#[derive(Debug, Clone)]
-struct AddTransaction {
-    accounts: Vec<fm_core::account::Account>,
-    categories: Vec<fm_core::Category>,
-    filter: Option<fm_core::transaction_filter::TransactionFilter>,
-    transactions: Vec<fm_core::Transaction>,
-}
+mod add_transaction {
+    use async_std::sync::Mutex;
+    use std::sync::Arc;
 
-impl AddTransaction {
-    async fn fetch(
-        finance_manager: Arc<Mutex<impl fm_core::FinanceManager + 'static>>,
-    ) -> Result<Self> {
-        let locked_manager = finance_manager.lock().await;
-        let accounts = locked_manager.get_accounts().await?;
-        let categories = locked_manager.get_categories().await?;
-        Ok(Self {
-            accounts,
-            categories,
-            filter: Some(fm_core::transaction_filter::TransactionFilter::default()),
-            transactions: Vec::new(),
-        })
+    use anyhow::Result;
+
+    use iced::widget;
+
+    use crate::utils;
+
+    pub enum Action {
+        Escape,
+        AddTransaction(fm_core::Transaction),
+        Task(iced::Task<Message>),
+        None,
     }
 
-    fn view(&self) -> iced::Element<Message> {
-        widget::column![
-            utils::heading("Add Transaction to Bill", utils::HeadingLevel::H1),
-            widget::button("Back to Bill Edit").on_press(Message::AddTransactionToggle),
-            if let Some(filter) = &self.filter {
-                widget::column![
-                    "Create Filter for Transactions:",
-                    utils::FilterComponent::new(
-                        filter.clone(),
-                        Message::AddTransactionFilterSubmit,
-                        &self.accounts,
-                        &self.categories,
-                    )
-                    .into_element()
-                ]
-                .spacing(10)
-                .width(iced::Length::Fill)
-                .into()
-            } else {
-                utils::TableView::new(self.transactions.clone(), |x| {
-                    [
-                        widget::button("Add")
-                            .on_press(Message::AddTransaction(x.clone()))
-                            .into(),
-                        widget::text(x.title().clone()).into(),
-                        widget::text(x.amount().to_num_string()).into(),
-                        widget::text(x.date().format("%d.%m.%Y").to_string()).into(),
-                    ]
-                })
-                .headers([
-                    "".to_string(),
-                    "Title".to_string(),
-                    "Amount".to_string(),
-                    "Date".to_string(),
-                ])
-                .sort_by(|a, b, column| match column {
-                    1 => a.title().cmp(&b.title()),
-                    2 => a.amount().cmp(&b.amount()),
-                    3 => a.date().cmp(&b.date()),
-                    _ => panic!(),
-                })
-                .columns_sortable([false, true, true, true])
-                .into_element()
+    #[derive(Debug, Clone)]
+    pub enum Message {
+        Back,
+        FilterSubmit(fm_core::transaction_filter::TransactionFilter),
+        AddTransaction(fm_core::Transaction),
+        FetchedTransactions(Vec<fm_core::Transaction>),
+    }
+
+    #[derive(Debug, Clone)]
+    pub struct AddTransaction {
+        accounts: Vec<fm_core::account::Account>,
+        categories: Vec<fm_core::Category>,
+        filter: Option<fm_core::transaction_filter::TransactionFilter>,
+        transactions: Vec<fm_core::Transaction>,
+        ignored_transactions: Vec<fm_core::Id>,
+    }
+
+    impl AddTransaction {
+        pub async fn fetch(
+            finance_manager: Arc<Mutex<impl fm_core::FinanceManager + 'static>>,
+            ignored_transactions: Vec<fm_core::Id>,
+        ) -> Result<Self> {
+            let locked_manager = finance_manager.lock().await;
+            let accounts = locked_manager.get_accounts().await?;
+            let categories = locked_manager.get_categories().await?;
+            Ok(Self {
+                accounts,
+                categories,
+                filter: Some(fm_core::transaction_filter::TransactionFilter::default()),
+                transactions: Vec::new(),
+                ignored_transactions,
+            })
+        }
+
+        pub fn update(
+            &mut self,
+            message: Message,
+            finance_manager: Arc<Mutex<impl fm_core::FinanceManager + 'static>>,
+        ) -> Action {
+            match message {
+                Message::Back => Action::Escape,
+                Message::FilterSubmit(filter) => {
+                    self.filter = None;
+                    Action::Task(iced::Task::future(async move {
+                        let locked_manager = finance_manager.lock().await;
+                        Message::FetchedTransactions(
+                            locked_manager
+                                .get_filtered_transactions(filter.clone())
+                                .await
+                                .unwrap(),
+                        )
+                    }))
+                }
+                Message::AddTransaction(transaction) => {
+                    self.ignored_transactions.push(*transaction.id());
+                    self.transactions.retain(|x| x.id() != transaction.id());
+                    Action::AddTransaction(transaction)
+                }
+                Message::FetchedTransactions(transactions) => {
+                    self.transactions = transactions;
+                    self.transactions.retain(|x| {
+                        for id in &self.ignored_transactions {
+                            if x.id() == id {
+                                return false;
+                            }
+                        }
+                        true
+                    });
+                    Action::None
+                }
             }
-        ]
-        .spacing(10)
-        .width(iced::Length::Fill)
-        .into()
+        }
+
+        pub fn view(&self) -> iced::Element<Message> {
+            widget::column![
+                utils::heading("Add", utils::HeadingLevel::H1),
+                widget::button("Back").on_press(Message::Back),
+                if let Some(filter) = &self.filter {
+                    widget::column![
+                        "Create Filter for Transactions:",
+                        utils::FilterComponent::new(
+                            filter.clone(),
+                            Message::FilterSubmit,
+                            &self.accounts,
+                            &self.categories,
+                        )
+                        .into_element()
+                    ]
+                    .spacing(10)
+                    .width(iced::Length::Fill)
+                    .into()
+                } else {
+                    utils::TableView::new(self.transactions.clone(), |x| {
+                        [
+                            widget::button("Add")
+                                .on_press(Message::AddTransaction(x.clone()))
+                                .into(),
+                            widget::text(x.title().clone()).into(),
+                            widget::text(x.amount().to_num_string()).into(),
+                            widget::text(x.date().format("%d.%m.%Y").to_string()).into(),
+                        ]
+                    })
+                    .headers([
+                        "".to_string(),
+                        "Title".to_string(),
+                        "Amount".to_string(),
+                        "Date".to_string(),
+                    ])
+                    .sort_by(|a, b, column| match column {
+                        1 => a.title().cmp(&b.title()),
+                        2 => a.amount().cmp(&b.amount()),
+                        3 => a.date().cmp(&b.date()),
+                        _ => panic!(),
+                    })
+                    .columns_sortable([false, true, true, true])
+                    .into_element()
+                }
+            ]
+            .spacing(10)
+            .width(iced::Length::Fill)
+            .into()
+        }
     }
 }
