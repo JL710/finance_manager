2796ab6e352a4a6265aff8a03bfdb51b98009224
JL710 <76447362+JL710@users.noreply.github.com>
2025-02-08 00:54:47 +0100

    unit tests for rest api

---
+++ fm_server/src/test.rs
@@ -0,0 +1,39 @@
+#[cfg(test)]
+mod test {
+    use crate::client::Client;
+    use crate::server::run_with_listener;
+    use fm_core::FinanceManager;
+    use tokio::net::TcpListener;
+
+    fn run_in_tokio_context(f: impl std::future::Future<Output = ()>) {
+        match tokio::runtime::Handle::try_current() {
+            Ok(handle) => handle.block_on(async {
+                f.await;
+            }),
+            Err(_) => {
+                let rt = tokio::runtime::Runtime::new().unwrap();
+                rt.handle().block_on(async { f.await });
+            }
+        };
+    }
+
+    async fn test_runner<FT: std::future::Future<Output = ()>>(
+        test: impl Fn(crate::client::Client) -> FT,
+    ) {
+        run_in_tokio_context(async {
+            let listener = TcpListener::bind("127.0.0.1:0").await.unwrap(); // Bind to any free port
+            let server_address = listener.local_addr().unwrap().to_string();
+
+            tokio::spawn(async {
+                run_with_listener(listener, None, "1234".to_string()).await;
+            });
+
+            println!("{}", server_address);
+
+            test(Client::new((format!("http://{}", server_address), "1234".to_string())).unwrap())
+                .await;
+        });
+    }
+
+    fm_core::finance_manager_test::unit_tests!(test_runner);
+}
