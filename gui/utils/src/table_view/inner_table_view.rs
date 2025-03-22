d2d8108946469585be153145f2296c3568ce424c
JL710 <76447362+JL710@users.noreply.github.com>
2025-03-22 21:25:04 +0100

    add InnerTableView state for caching column width

--- gui/utils/src/table_view/inner_table_view.rs
+++ gui/utils/src/table_view/inner_table_view.rs
@@ -1,5 +1,9 @@
 use iced::advanced::Renderer;
 
+struct State {
+    column_widths: Option<Vec<f32>>,
+}
+
 pub struct InnerTableView<'a, Message, const COLUMNS: usize> {
     /// name and sort svg/button represents one header
     header_elements: Vec<(iced::Element<'a, Message>, iced::Element<'a, Message>)>,
@@ -14,6 +18,7 @@ pub struct InnerTableView<'a, Message, const COLUMNS: usize> {
 }
 
 impl<'a, Message, const COLUMNS: usize> InnerTableView<'a, Message, COLUMNS> {
+    #[allow(clippy::too_many_arguments)]
     pub fn new(
         header_elements: Vec<(iced::Element<'a, Message>, iced::Element<'a, Message>)>,
         elements: Vec<iced::Element<'a, Message>>,
@@ -71,10 +76,10 @@ fn max_node_width(nodes: &[iced::advanced::layout::Node]) -> f32 {
     smallest
 }
 
-fn layouts_max<'a>(
+fn layouts_max(
     start_x: f32,
     start_y: f32,
-    layouts: &[iced::advanced::layout::Layout<'a>],
+    layouts: &[iced::advanced::layout::Layout<'_>],
 ) -> (f32, f32) {
     let mut x: f32 = start_x;
     let mut y: f32 = start_y;
@@ -122,12 +127,12 @@ fn unpositioned_header_layouts<'a, Message>(
     (nodes, widths)
 }
 
-fn unpositioned_cell_layouts<'a, Message, const COLUMNS: usize>(
-    table: &InnerTableView<'a, Message, COLUMNS>,
+fn unpositioned_cell_layouts<Message, const COLUMNS: usize>(
+    table: &InnerTableView<'_, Message, COLUMNS>,
     renderer: &iced::Renderer,
     states: &mut [iced::advanced::widget::Tree],
     reserved_space: f32,
-) -> ([Vec<iced::advanced::layout::Node>; COLUMNS], Vec<f32>) {
+) -> (Vec<iced::advanced::layout::Node>, Vec<f32>) {
     let mut column_layouts = [const { Vec::new() }; COLUMNS];
     let mut column_widths: [Option<f32>; COLUMNS] = [None; COLUMNS];
 
@@ -148,14 +153,9 @@ fn unpositioned_cell_layouts<'a, Message, const COLUMNS: usize>(
         let mut max_width = table.max_column_sizes[column_index];
         // if max is weak expand max_width if possible
         if weak_max {
-            let space_left = (0.0 as f32).max(
+            let space_left = (0.0_f32).max(
                 table.optimal_width
-                    - (reserved_space as f32
-                        + column_widths
-                            .iter()
-                            .filter(|x| x.is_some())
-                            .map(|x| x.unwrap())
-                            .sum::<f32>()),
+                    - (reserved_space + column_widths.iter().flatten().sum::<f32>()),
             );
             if space_left > 0.0 {
                 let spaced_needed = column_widths
@@ -187,15 +187,28 @@ fn unpositioned_cell_layouts<'a, Message, const COLUMNS: usize>(
         column_widths[column_index] = Some(max_node_width(&column_layouts[column_index]));
     }
 
+    let mut cell_layouts = Vec::with_capacity(table.elements.len());
+    for row_index in 0..(table.elements.len() / COLUMNS) {
+        for column_layout in column_layouts.iter().take(COLUMNS) {
+            cell_layouts.push(column_layout[row_index].clone());
+        }
+    }
+
     (
-        column_layouts,
+        cell_layouts,
         column_widths.iter().map(|x| x.unwrap()).collect::<Vec<_>>(),
     )
 }
 
-impl<'a, Message, const COLUMNS: usize> iced::advanced::Widget<Message, iced::Theme, iced::Renderer>
-    for InnerTableView<'a, Message, COLUMNS>
+impl<Message, const COLUMNS: usize> iced::advanced::Widget<Message, iced::Theme, iced::Renderer>
+    for InnerTableView<'_, Message, COLUMNS>
 {
+    fn state(&self) -> iced::advanced::widget::tree::State {
+        iced::advanced::widget::tree::State::new(State {
+            column_widths: None,
+        })
+    }
+
     fn size(&self) -> iced::Size<iced::Length> {
         iced::Size::new(iced::Shrink, iced::Shrink)
     }
@@ -208,6 +221,17 @@ impl<'a, Message, const COLUMNS: usize> iced::advanced::Widget<Message, iced::Th
     }
 
     fn diff(&self, tree: &mut iced::advanced::widget::Tree) {
+        let state = tree.state.downcast_mut::<State>();
+
+        let state_is_valid = if let Some(column_widths) = &mut state.column_widths {
+            column_widths.len() == COLUMNS
+        } else {
+            true
+        };
+        if !state_is_valid {
+            state.column_widths = None;
+        }
+
         tree.diff_children(&self.child_elements());
     }
 
@@ -217,29 +241,79 @@ impl<'a, Message, const COLUMNS: usize> iced::advanced::Widget<Message, iced::Th
         renderer: &iced::Renderer,
         _limits: &iced::advanced::layout::Limits,
     ) -> iced::advanced::layout::Node {
+        let state = tree.state.downcast_mut::<State>();
+
         let rows = self.elements.len() / COLUMNS;
         let horizontal_padding_sum = (COLUMNS - 1) as f32 * self.column_spacing;
 
-        let (header_layouts, header_widths) = unpositioned_header_layouts(
-            &self.header_elements,
-            &self.max_column_sizes,
-            renderer,
-            &mut tree.children[0..(self.header_elements.len() * 2)],
-        );
+        let mut child_layouts = Vec::with_capacity(COLUMNS * 2 + COLUMNS * rows);
 
-        let (column_layouts, element_column_widths) = unpositioned_cell_layouts(
-            self,
-            renderer,
-            &mut tree.children[self.header_elements.len() * 2..],
-            horizontal_padding_sum,
-        );
+        let column_widths = if let Some(col_widths) = state.column_widths.clone() {
+            let mut child_state_iterator = tree.children.iter_mut();
 
-        // calculate column_widths
-        let mut column_widths = Vec::with_capacity(COLUMNS);
-        for column_index in 0..COLUMNS {
+            for (column_index, column_width) in col_widths.iter().enumerate().take(COLUMNS) {
+                child_layouts.push(self.header_elements[column_index].0.as_widget().layout(
+                    child_state_iterator.next().unwrap(),
+                    renderer,
+                    &iced::advanced::layout::Limits::new(
+                        iced::Size::new(0.0, 0.0),
+                        iced::Size::new(*column_width, f32::INFINITY),
+                    ),
+                ));
+                child_layouts.push(self.header_elements[column_index].1.as_widget().layout(
+                    child_state_iterator.next().unwrap(),
+                    renderer,
+                    &iced::advanced::layout::Limits::new(
+                        iced::Size::new(0.0, 0.0),
+                        iced::Size::new(*column_width, f32::INFINITY),
+                    ),
+                ));
+            }
+
+            for row_index in 0..rows {
+                for (column_index, column_width) in col_widths.iter().enumerate().take(COLUMNS) {
+                    child_layouts.push(
+                        self.elements[row_index * COLUMNS + column_index]
+                            .as_widget()
+                            .layout(
+                                child_state_iterator.next().unwrap(),
+                                renderer,
+                                &iced::advanced::layout::Limits::new(
+                                    iced::Size::new(0.0, 0.0),
+                                    iced::Size::new(*column_width, f32::INFINITY),
+                                ),
+                            ),
+                    )
+                }
+            }
+
+            col_widths
+        } else {
+            let (header_layouts, header_widths) = unpositioned_header_layouts(
+                &self.header_elements,
+                &self.max_column_sizes,
+                renderer,
+                &mut tree.children[0..(self.header_elements.len() * 2)],
+            );
+            child_layouts.extend(header_layouts);
+
+            let (cell_layouts, element_column_widths) = unpositioned_cell_layouts(
+                self,
+                renderer,
+                &mut tree.children[self.header_elements.len() * 2..],
+                horizontal_padding_sum,
+            );
+            child_layouts.extend(cell_layouts);
+
+            // calculate column_widths
+            let mut column_widths = Vec::with_capacity(COLUMNS);
+            for column_index in 0..COLUMNS {
+                column_widths
+                    .push(header_widths[column_index].max(element_column_widths[column_index]));
+            }
+
             column_widths
-                .push(header_widths[column_index].max(element_column_widths[column_index]));
-        }
+        };
 
         // calculate column start/x positions
         let mut column_start_positions = Vec::with_capacity(COLUMNS);
@@ -254,54 +328,48 @@ impl<'a, Message, const COLUMNS: usize> iced::advanced::Widget<Message, iced::Th
         }
 
         // generate child layouts
-        let mut child_layouts = Vec::with_capacity(header_layouts.len() + self.elements.len());
         let mut total_height: f32 = 0.0 + self.cell_padding.top;
+        let mut child_layout_iterator = child_layouts.iter_mut();
 
         // generate header layouts
         for column_index in 0..COLUMNS {
-            let layout_left = header_layouts[column_index * 2]
-                .clone()
-                .move_to((column_start_positions[column_index], self.cell_padding.top));
-            let mut layout_right = header_layouts[column_index * 2 + 1].clone();
+            let layout_left = child_layout_iterator.next().unwrap();
+            layout_left.move_to_mut((column_start_positions[column_index], self.cell_padding.top));
+
+            let layout_right = child_layout_iterator.next().unwrap();
             let space_between_them =
                 column_widths[column_index] - layout_left.size().width - layout_right.size().width;
-            layout_right = layout_right.clone().move_to((
+            layout_right.move_to_mut((
                 column_start_positions[column_index] + layout_left.size().width
                 // make a space between both as bit as possible
                 + space_between_them,
                 self.cell_padding.top,
             ));
 
-            child_layouts.push(layout_left);
-            child_layouts.push(layout_right);
-
             total_height = total_height
-                .max(header_layouts[column_index * 2].size().height + self.cell_padding.vertical())
-                .max(
-                    header_layouts[column_index * 2 + 1].size().height
-                        + self.cell_padding.vertical(),
-                )
+                .max(layout_left.size().height + self.cell_padding.vertical())
+                .max(layout_right.size().height + self.cell_padding.vertical())
         }
 
         // generate cell layouts
-        for row_index in 0..rows {
+        for _ in 0..rows {
             let y = total_height + self.row_spacing + self.cell_padding.vertical();
-            for column_index in 0..COLUMNS {
-                if total_height < y + column_layouts[column_index][row_index].size().height {
-                    total_height = y + column_layouts[column_index][row_index].size().height;
+            for (column_index, cell_layout) in
+                (&mut child_layout_iterator).take(COLUMNS).enumerate()
+            {
+                if total_height < y + cell_layout.size().height {
+                    total_height = y + cell_layout.size().height;
                 }
-                child_layouts.push(
-                    column_layouts[column_index][row_index]
-                        .clone() // FIXME: remote this clone if possible
-                        .move_to(iced::Point::new(column_start_positions[column_index], y)),
-                );
+                cell_layout.move_to_mut(iced::Point::new(column_start_positions[column_index], y));
             }
         }
 
+        state.column_widths = Some(column_widths);
+
         iced::advanced::layout::Node::with_children(
             iced::Size::new(
-                horizontal_padding_sum as f32
-                    + column_widths.iter().sum::<f32>()
+                horizontal_padding_sum
+                    + state.column_widths.as_ref().unwrap().iter().sum::<f32>()
                     + COLUMNS as f32 * self.cell_padding.horizontal(),
                 total_height,
             ),
@@ -319,6 +387,12 @@ impl<'a, Message, const COLUMNS: usize> iced::advanced::Widget<Message, iced::Th
         cursor: iced::advanced::mouse::Cursor,
         viewport: &iced::Rectangle,
     ) {
+        let state = tree.state.downcast_ref::<State>();
+
+        let row_width = state.column_widths.as_ref().unwrap().iter().sum::<f32>()
+            + COLUMNS as f32 * self.cell_padding.horizontal()
+            + 0.0_f32.max((COLUMNS - 1) as f32) * self.column_spacing;
+
         let mut child_draw_todos = self
             .child_elements()
             .into_iter()
@@ -328,7 +402,7 @@ impl<'a, Message, const COLUMNS: usize> iced::advanced::Widget<Message, iced::Th
 
         // draw heading border
         let header_todos = pop_front_slice(&mut child_draw_todos, COLUMNS * 2);
-        let (max_header_border_x, max_header_border_y) = layouts_max(
+        let (_, max_header_border_y) = layouts_max(
             layout.position().x,
             layout.position().y,
             &header_todos.iter().map(|x| x.1).collect::<Vec<_>>(),
@@ -338,10 +412,7 @@ impl<'a, Message, const COLUMNS: usize> iced::advanced::Widget<Message, iced::Th
                 bounds: iced::Rectangle::new(
                     layout.position(),
                     (
-                        self.optimal_width.max(
-                            max_header_border_x - layout.position().x
-                                + self.cell_padding.horizontal(),
-                        ), // use optimal width or max header x depending on what is larger
+                        self.optimal_width.max(row_width), // use optimal width or max header x depending on what is larger
                         max_header_border_y - layout.position().y + self.cell_padding.bottom,
                     )
                         .into(),
@@ -368,7 +439,7 @@ impl<'a, Message, const COLUMNS: usize> iced::advanced::Widget<Message, iced::Th
         while !child_draw_todos.is_empty() {
             let y_cell_start = child_draw_todos[0].1.position().y;
             let row_todos = pop_front_slice(&mut child_draw_todos, COLUMNS);
-            let (max_row_border_x, max_row_border_y) = layouts_max(
+            let (_, max_row_border_y) = layouts_max(
                 layout.position().x,
                 layout.position().y,
                 &row_todos.iter().map(|x| x.1).collect::<Vec<_>>(),
@@ -378,10 +449,7 @@ impl<'a, Message, const COLUMNS: usize> iced::advanced::Widget<Message, iced::Th
                     bounds: iced::Rectangle::new(
                         (layout.position().x, y_cell_start - self.cell_padding.top).into(),
                         (
-                            self.optimal_width.max(
-                                max_row_border_x - layout.position().x
-                                    + self.cell_padding.horizontal(),
-                            ), // use optimal width or max header x depending on what is larger
+                            self.optimal_width.max(row_width), // use optimal width or max header x depending on what is larger
                             max_row_border_y - y_cell_start + self.cell_padding.vertical(),
                         )
                             .into(),
